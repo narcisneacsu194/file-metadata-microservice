e16d37b8fd362a03f474efb8eee7b27705d493ee
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-12-03 13:21:25 +0200

    Initial commit.

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const multer = require('multer');
+const port = process.env.PORT || 3000;
+const app = express();
+const upload = multer();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.redirect('/fileForm.html');
+});
+
+app.post('/api/fileanalyse', upload.single('newfile'), (req, res) => {
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server started using port ${port}.`);
+});
