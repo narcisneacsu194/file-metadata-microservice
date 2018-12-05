5f163f78a56d9019a82698a386dfbce43a2ffa9f
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-12-05 18:54:53 +0200

    Added HelmetJS security middleware functions to express.

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const helmet = require('helmet');
 
 const port = process.env.PORT || 3000;
 const app = express();
 const upload = multer();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({ maxAge: 7776000, force: true }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com'],
+  },
+}));
 
 app.get('/', (req, res) => {
   res.redirect('/fileForm.html');
